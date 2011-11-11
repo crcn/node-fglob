fa2a5ba8012506c034d74688a340d1a3a593a5c4
Craig Condon <craig@spiceapps.com>
2011-11-10 22:14:42 -0600

    initial push

---
+++ src/index.js
@@ -0,0 +1,83 @@
+var minimatch = require('minimatch'),
+fs = require('fs');
+
+
+function match(paths, cwd, files, statd)
+{
+	if(!files) files = [];
+	
+	if(!paths.length)
+	{
+		try
+		{
+			//throws error if file does not exist
+			if(!statd) fs.statSync(cwd);
+			
+			files[cwd.substr(1)] = 1;
+		}catch(e)
+		{
+			
+		}
+		return;
+	}
+	
+	
+	var path = paths.shift();
+	
+	
+	if(path.indexOf('*') > -1)
+	{
+		var regex = new RegExp('^'+path.replace(/\./g,'\\.').replace(/\*/g,'.*?') + '$');
+		
+		
+		var stat = fs.statSync(cwd);
+		                    
+		if(stat.isDirectory())
+		{
+			fs.readdirSync(cwd).forEach(function(file)
+			{
+				if(regex.test(file))
+				{
+					match(paths.concat(), cwd + '/' + file, files, true);
+				}
+			});
+		}
+	}
+	else
+	{
+		cwd += '/' + path;
+		
+		return match(paths.concat(), cwd, files);
+	}
+	
+	return files;
+}
+ 
+module.exports = function(includes, callback)
+{
+	var search = includes.split(/[\s,]+/g),
+	numSearching = search.length,
+	allFiles = {};
+	
+	search.forEach(function(path)
+	{
+		path = path.replace(/~/g, process.env.HOME);
+		
+		if(path.substr(0,1) != '/') path += process.cwd() + '/' + path; 
+		
+		var paths = path.split('/');
+		
+		paths.shift();
+		
+		match(paths, '/', allFiles);
+	});
+	
+	var ar = [];
+	
+	for(var file in allFiles)
+	{
+		ar.push(file);
+	}
+	
+	callback(ar);
+}
